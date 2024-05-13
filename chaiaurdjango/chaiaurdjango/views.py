6104eaffe8639785f17803a32e4cf152b7818c5a
Baber Ibrar <baberibrar@gmail.com>
2024-05-13 22:16:11 +0500

    Integrated template, added static files

--- chaiaurdjango/chaiaurdjango/views.py
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("<h1>Welcome to Chai's Django Project: Home page</h1>")
+    return render(request, 'index.html')
 
 
 def about(request):
